b9d1896b6b3fb46ffbb7d3b4635180f22daa2afe
Chelley <michelle.owodunni@gmail.com>
2019-11-25 23:39:47 -0600

    card updates

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,3 @@
-// "2019-11-27 03:00:00".split(" ")
-// (2) ["2019-11-27", "03:00:00"]
-
 let city = "";
 
 // add event listener to search button
@@ -53,12 +50,12 @@ searchButton.addEventListener("click", function(event) {
         axios.get(queryURL)
             // After the data comes back from the API
             .then(function(response) {
-                console.log(response)
+                // console.log(response)
                 let image = response.data.weather[0].icon;
                 let iconURL = "http://openweathermap.org/img/wn/" + image + "@2x.png";
-                console.log(iconURL)
+                // console.log(iconURL)
                 const imgEl = document.querySelector("img");
-                console.log(imgEl)
+                // console.log(imgEl)
                 let displayImage = imgEl.setAttribute("src", iconURL);
                 // displayImage.innerText
 
@@ -71,7 +68,7 @@ searchButton.addEventListener("click", function(event) {
                 let queryURL3 = "http://api.openweathermap.org/data/2.5/uvi?appid=7a8635b4bf69d952fca178d66748f81f&lat=" + lat + "&lon=" + lon;
                 axios.get(queryURL3)
                     .then(function(uvResponse) {
-                        console.log(uvResponse);
+                        // console.log(uvResponse);
                         document.getElementById("uv").innerHTML = "UV Index: " + uvResponse.data.value;
                     })
                     // Storing an array of results in the results variable
@@ -99,7 +96,6 @@ searchButton.addEventListener("click", function(event) {
             })
 
         let queryURL2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=7a8635b4bf69d952fca178d66748f81f";
-        console.log(queryURL2);
 
         axios.get(queryURL2)
             .then(function(response) {
@@ -107,11 +103,30 @@ searchButton.addEventListener("click", function(event) {
                 const forecastArray = response.data.list;
                 // console.log(response);
                 let day1 = forecastArray[0];
-                // console.log(forecastArray[8].weather[0].icon);
+                let date1 = day1.dt_txt;
+                let icon1 = day1.weather[0].icon;
+                let temp1 = day1.main.temp;
+                let humidity1 = day1.main.humidity;
+
+                let array1 = date1.split(" ");
+
+                let iconURL = "http://openweathermap.org/img/wn/" + icon1 + "@2x.png";
+
+                document.getElementById("date1").innerHTML = array1[0] + "<img src=" + iconURL + ">";
+                document.getElementById("temp1").innerHTML = "Humidity: " + temp1;
+                document.getElementById("humidity1").innerHTML = "Temp (F) " + humidity1;
+                console.log(icon1)
+                    // console.log(forecastArray[8].weather[0].icon);
                 let day2 = forecastArray[8];
                 let day3 = forecastArray[16];
                 let day4 = forecastArray[24];
                 let day5 = forecastArray[32];
+                console.log(day1);
+                console.log(day2);
+
+
+                // "2019-11-27 03:00:00".split(" ")
+                // (2) ["2019-11-27", "03:00:00"]
 
                 // document.getElementById("city").innerHTML = "<h1>" + results.name + " Weather Details</h1>";
                 // document.getElementById("wind").innerHTML = "Wind Speed: " + response.data.wind.speed;
